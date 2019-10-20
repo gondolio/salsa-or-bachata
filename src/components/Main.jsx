849ec0f326bc6f7a95bcf4bb59344215a1a1250a
gondolio <oren.sivan@gmail.com>
2019-10-20 11:36:00 +0200

    add react-ga for google analytics

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import ReactGA from 'react-ga'; // Google Analytics
 import Game from './Game';
 
 function Main() {
+  ReactGA.initialize('UA-150417728-1');
+  ReactGA.pageview('/main');
   return (
     <div className="App">
       <Game />
