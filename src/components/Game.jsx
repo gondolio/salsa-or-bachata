6903d4004072436e36c6c3a31d28da1d5a9071cc
gondolio <oren.sivan@gmail.com>
2019-10-25 19:36:40 +0200

    Add <OptionsModal> after <PlayScreen>

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -45,10 +45,23 @@ class Game extends React.Component {
   render() {
     if (this.state.gameState === 'playing') {
       return (
-        <PlayScreen
-          handleGameState={this.handleGameState}
-          genreIsEnabled={this.state.genreIsEnabled}
-        />
+        <>
+          <PlayScreen
+            handleGameState={this.handleGameState}
+            genreIsEnabled={this.state.genreIsEnabled}
+            key={JSON.stringify(this.state.genreIsEnabled)}
+            /* OptionsModal can change this.state.genreIsEnabled
+               When this happens we want PlayScreen to be reset.
+               Key therefore uses this.state.genreIsEnabled because
+               when a key changes, React will create a new component instance
+               rather than update the current one (see link below)
+               https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html#recommendation-fully-controlled-component */
+          />
+          <OptionsModal
+            genreIsEnabled={this.state.genreIsEnabled}
+            setEnabledGenres={this.setEnabledGenres}
+          />
+        </>
       );
     }
 
